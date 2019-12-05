96135d7351712927cd8f8d9dbb8e02dc08dde855
Edward Robayo <edwardd_22@hotmail.com>
2019-12-04 22:31:04 -0500

    Vista pdf,xlsx y csv hasta Clase 172

---
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/view/csv/ClienteCsvView.java
@@ -0,0 +1,53 @@
+package com.bolsadeideas.springboot.app.view.csv;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.AbstractView;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
+
+import com.bolsadeideas.springboot.app.models.entity.Cliente;
+
+@Component("listar")
+public class ClienteCsvView extends AbstractView{
+	
+	public ClienteCsvView() {
+		setContentType("text/csv");
+	}
+
+	@Override
+	protected boolean generatesDownloadContent() {
+		return true;
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		response.setHeader("Content-Disposition", "attachment; filename=\"clientes.csv\"");
+		response.setContentType(getContentType());
+		
+		List<Cliente> clientes = (List<Cliente>) model.get("clientes");
+		
+		//El beanwriter se encarga de leer un objeto Bean simplemente se necesita especificar un header que especifica que columnas
+		//deberan ser incluidas en el archivo y el beanwriter se encargara de llamar a los respectivos metodos
+		//getter y setter
+		ICsvBeanWriter beanWriter = new CsvBeanWriter(response.getWriter(), CsvPreference.STANDARD_PREFERENCE);
+		
+		String[] header = {"id", "nombre","apellido","email","createAt"};
+		beanWriter.writeHeader(header);
+		
+		for(Cliente cliente: clientes) {
+			beanWriter.write(cliente, header);
+		}
+		
+		beanWriter.close();
+	}
+
+}
